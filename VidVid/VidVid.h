9ceef1d6c8221eb817ae046b8f127d2bc0b95a03
Fabian Canas <fcanas@gmail.com>
2016-03-18 21:10:38 -0400

    Initial commit

---
+++ VidVid/VidVid.h
@@ -0,0 +1,19 @@
+//
+//  VidVid.h
+//  VidVid
+//
+//  Created by Fabian Canas on 3/18/16.
+//  Copyright © 2016 Fabián Cañas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VidVid.
+FOUNDATION_EXPORT double VidVidVersionNumber;
+
+//! Project version string for VidVid.
+FOUNDATION_EXPORT const unsigned char VidVidVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VidVid/PublicHeader.h>
+
+
---
+++ VidVidTests/VidVidTests.m
@@ -0,0 +1,39 @@
+//
+//  VidVidTests.m
+//  VidVidTests
+//
+//  Created by Fabian Canas on 3/18/16.
+//  Copyright © 2016 Fabián Cañas. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VidVidTests : XCTestCase
+
+@end
+
+@implementation VidVidTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
